d234b00f17d77bd26e258c13667195670324a2bf
celesteorellana1 <sintiaorellana1@gmail.com>
2024-05-04 12:52:49 -0600

    primer commit

---
+++ Windows Form/CalculadoraPrestamo/CalculadoraPrestamo/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace CalculadoraPrestamo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtMonto = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtInteres = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TxtPlazo = new System.Windows.Forms.TextBox();
+            this.BtnCalcular = new System.Windows.Forms.Button();
+            this.BtnLimpiar = new System.Windows.Forms.Button();
+            this.BtnSalir = new System.Windows.Forms.Button();
+            this.LblResultado = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TxtMensual = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Himalaya", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(224, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(326, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculadora de Cuotas de un Prestamo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Himalaya", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(333, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Monto Prestamo";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // TxtMonto
+            // 
+            this.TxtMonto.Location = new System.Drawing.Point(312, 95);
+            this.TxtMonto.Name = "TxtMonto";
+            this.TxtMonto.Size = new System.Drawing.Size(143, 22);
+            this.TxtMonto.TabIndex = 2;
+            this.TxtMonto.TextChanged += new System.EventHandler(this.TxtMonto_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Himalaya", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(333, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Tasa de Interes";
+            // 
+            // TxtInteres
+            // 
+            this.TxtInteres.Location = new System.Drawing.Point(312, 148);
+            this.TxtInteres.Name = "TxtInteres";
+            this.TxtInteres.Size = new System.Drawing.Size(143, 22);
+            this.TxtInteres.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Himalaya", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(317, 182);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(138, 25);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Plazo del Prestamo";
+            // 
+            // TxtPlazo
+            // 
+            this.TxtPlazo.Location = new System.Drawing.Point(309, 210);
+            this.TxtPlazo.Name = "TxtPlazo";
+            this.TxtPlazo.Size = new System.Drawing.Size(143, 22);
+            this.TxtPlazo.TabIndex = 6;
+            // 
+            // BtnCalcular
+            // 
+            this.BtnCalcular.Location = new System.Drawing.Point(208, 312);
+            this.BtnCalcular.Name = "BtnCalcular";
+            this.BtnCalcular.Size = new System.Drawing.Size(90, 33);
+            this.BtnCalcular.TabIndex = 7;
+            this.BtnCalcular.Text = "Calcular";
+            this.BtnCalcular.UseVisualStyleBackColor = true;
+            this.BtnCalcular.Click += new System.EventHandler(this.BtnCalcular_Click);
+            // 
+            // BtnLimpiar
+            // 
+            this.BtnLimpiar.Location = new System.Drawing.Point(436, 312);
+            this.BtnLimpiar.Name = "BtnLimpiar";
+            this.BtnLimpiar.Size = new System.Drawing.Size(90, 34);
+            this.BtnLimpiar.TabIndex = 8;
+            this.BtnLimpiar.Text = "Limpiar";
+            this.BtnLimpiar.UseVisualStyleBackColor = true;
+            this.BtnLimpiar.Click += new System.EventHandler(this.BtnLimpiar_Click);
+            // 
+            // BtnSalir
+            // 
+            this.BtnSalir.Location = new System.Drawing.Point(322, 394);
+            this.BtnSalir.Name = "BtnSalir";
+            this.BtnSalir.Size = new System.Drawing.Size(90, 33);
+            this.BtnSalir.TabIndex = 9;
+            this.BtnSalir.Text = "Salir";
+            this.BtnSalir.UseVisualStyleBackColor = true;
+            this.BtnSalir.Click += new System.EventHandler(this.BtnSalir_Click);
+            // 
+            // LblResultado
+            // 
+            this.LblResultado.AutoSize = true;
+            this.LblResultado.Location = new System.Drawing.Point(183, 354);
+            this.LblResultado.Name = "LblResultado";
+            this.LblResultado.Size = new System.Drawing.Size(63, 16);
+            this.LblResultado.TabIndex = 10;
+            this.LblResultado.Text = "resultado";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Himalaya", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(333, 235);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(119, 25);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Interes Mensual";
+            // 
+            // TxtMensual
+            // 
+            this.TxtMensual.Location = new System.Drawing.Point(309, 263);
+            this.TxtMensual.Name = "TxtMensual";
+            this.TxtMensual.Size = new System.Drawing.Size(143, 22);
+            this.TxtMensual.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TxtMensual);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.LblResultado);
+            this.Controls.Add(this.BtnSalir);
+            this.Controls.Add(this.BtnLimpiar);
+            this.Controls.Add(this.BtnCalcular);
+            this.Controls.Add(this.TxtPlazo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TxtInteres);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TxtMonto);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtMonto;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtInteres;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TxtPlazo;
+        private System.Windows.Forms.Button BtnCalcular;
+        private System.Windows.Forms.Button BtnLimpiar;
+        private System.Windows.Forms.Button BtnSalir;
+        private System.Windows.Forms.Label LblResultado;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox TxtMensual;
+    }
+}
+
---
+++ Windows Form/CalculadoraPrestamo/CalculadoraPrestamo/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraPrestamo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtMonto_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnCalcular_Click(object sender, EventArgs e)
+        {
+            double monto = double.Parse(TxtMonto.Text);
+            double interes = double.Parse(TxtInteres.Text);
+            double plazo = double.Parse(TxtPlazo.Text);
+            double interesMensual = interes / 12 / 100;
+
+            double Cuota = (monto * interes) / (1 - Math.Pow(1 + interesMensual, -plazo * 12));
+
+            LblResultado.Text = $" La cuota es de: {Cuota} ";
+        }
+
+        private void BtnLimpiar_Click(object sender, EventArgs e)
+        {
+            TxtInteres.Clear();
+            TxtMonto.Clear();
+            TxtPlazo.Clear();
+            TxtMensual.Clear();
+            LblResultado.Text = string.Empty;
+        }
+
+        private void BtnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
